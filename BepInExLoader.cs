6dd8ace78937fc749ff8be378525a92c4466bd79
Yentis <yentl178@gmail.com>
2024-11-16 12:57:51 +0100

    initial commit

---
+++ BepInExLoader.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BokuMono.Data;
+using HarmonyLib;
+using System;
+
+namespace SoSPioneersBlackScreen
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class BepInExLoader : BepInEx.Unity.IL2CPP.BasePlugin
+    {
+        public const string
+            MODNAME = "BlackScreenFix",
+            AUTHOR = "Yentis",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.1";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public BepInExLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            log.LogMessage("Loading");
+
+            try
+            {
+                var harmony = new Harmony("yentis.blackscreenfix.il2cpp");
+
+                harmony.Patch(
+                    typeof(NpcScheduleMasterData).GetMethod(nameof(NpcScheduleMasterData.GetNotOpenFestivalScheduleId)),
+                    prefix: new HarmonyMethod(typeof(Patches).GetMethod(nameof(Patches.GetNotOpenFestivalScheduleId)))
+                );
+            }
+            catch (Exception exception)
+            {
+                log.LogError(exception);
+            }
+
+            log.LogMessage("Started");
+        }
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,20 @@
+﻿using BokuMono;
+using BokuMono.Data;
+using UnityEngine;
+
+namespace SoSPioneersBlackScreen
+{
+    public class Patches : MonoBehaviour
+    {
+        public static bool GetNotOpenFestivalScheduleId(ref NpcScheduleMasterData __instance, ref uint __result, BokuMonoSeason season, int index)
+        {
+            if (__instance.CharaId >= 700)
+            {
+                __result = 0;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
